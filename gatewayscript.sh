40ca11b1b94135f69b8597a607cba23dd8bcd5e3
Christian Castillo <58743096+christiancastilloporras@users.noreply.github.com>
2022-03-31 13:33:46 -0600

    Update gatewayscript.sh

--- gatewayscript.sh
+++ gatewayscript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-clish -C "set static-route 10.110.0.4/32 nexthop gateway address 10.254.0.1 on" -s
+clish -c 'set static-route 10.110.0.4/32 nexthop gateway address 10.254.0.1 on' -s
 #fw ctl set int fw_daf_module_mac_mode 1
 #echo "fw_daf_module_mac_mode=1" >> $FWDIR/modules/fwkern.conf
 
